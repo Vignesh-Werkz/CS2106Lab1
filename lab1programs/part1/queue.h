77de339cef365213519bc73de8d6f2f10cebd985
Vignesh Bhuvan <vigneshbhuvan@hotmail.com>
2024-08-08 16:45:00 +0800

    Developed Flex_Reduce Method

--- lab1programs/part1/queue.h
+++ lab1programs/part1/queue.h
@@ -6,3 +6,9 @@
 // declare prototypes below
 void enq(double);
 double deq();
+double sum(double, double);
+double prod(double, double);
+void clear_sum(double*);
+void clear_prod(double*);
+double reduce(); 
+double flex_reduce(void (*)(double*), double (*)(double, double));
